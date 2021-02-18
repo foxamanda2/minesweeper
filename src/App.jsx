171690fabe7fee65ad1a4818312f425f32bdcbdc
foxamanda2 <fox.amanda2@gmail.com>
2021-02-18 16:37:53 -0500

    Table Made

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React, { Component } from 'react'
+import { Table } from './components/Table'
 
+// state = {
+//   board: [],
+//   state: "new",
+//   mines: 10
+// }
+// async componentBoardMount() {
+//   await fetch('https://minesweeper-api.herokuapp.com/').then(response=> (return response.json())).then (apidata=> (console.log(apiData)))
+// }
 export class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th colspan="2">MineSweeper</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Table />
+          <Table />
+          <Table />
+          <Table />
+          <Table />
+          <Table />
+          <Table />
+          <Table />
+          <Table />
+          <Table />
+        </tbody>
+      </table>
+    )
   }
 }
---
+++ src/components/Table.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Table extends Component {
+  render() {
+    return (
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    )
+  }
+}
