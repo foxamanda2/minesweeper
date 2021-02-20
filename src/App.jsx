0445d282b7f2a717e76c25df0d22541b1ae3b8fc
foxamanda2 <fox.amanda2@gmail.com>
2021-02-19 20:47:05 -0500

    Working Minesweeper

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,80 @@
 import React, { Component } from 'react'
 import { Table } from './components/Table'
 
-// state = {
-//   board: [],
-//   state: "new",
-//   mines: 10
-// }
-// async componentBoardMount() {
-//   await fetch('https://minesweeper-api.herokuapp.com/').then(response=> (return response.json())).then (apidata=> (console.log(apiData)))
-// }
 export class App extends Component {
+  state = {
+    // id: 1,
+    board: [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
+  }
+  handleClickCell = async (row, col) => {
+    const url = `https://minesweeper-api.herokuapp.com/games/${this.state.id}/check`
+    const body = { row: row, col: col }
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+    console.log(response)
+    const game = await response.json()
+    this.setState({ board: game.board })
+  }
+  handleFlagCell = async (row, col) => {
+    const url = `https://minesweeper-api.herokuapp.com/games/${this.state.id}/flag`
+    // for difficulty at the end of games?{difficuly}
+    const body = { row: row, col: col }
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+    console.log(response)
+    const game = await response.json()
+    this.setState({ board: game.board })
+  }
+  handleNewGame = async () => {
+    const response = await fetch(
+      'https://minesweeper-api.herokuapp.com/games',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+    const game = await response.json()
+    this.setState(game)
+  }
   render() {
     return (
       <table>
-        <thead>
-          <tr>
-            <th colspan="2">MineSweeper</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Table />
-          <Table />
-          <Table />
-          <Table />
-          <Table />
-          <Table />
-          <Table />
-          <Table />
-          <Table />
-          <Table />
+        <caption>
+          MineSweeper <button onClick={this.handleNewGame}>New Game</button>
+        </caption>
+        <tbody className="table">
+          {/* <tr> */}
+          {this.state.board.map((boardRow, rowIndex) => {
+            return (
+              <tr key={rowIndex}>
+                {boardRow.map((cell, colIndex) => {
+                  return (
+                    <td
+                      key={colIndex}
+                      onClick={() => this.handleClickCell(rowIndex, colIndex)}
+                    >
+                      {cell}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     )
